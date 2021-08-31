d10e4c8f3edcb4e1d245b0dbfbf665d7d875086a
sjy <ks2414e@naver.com>
2021-08-31 19:17:05 +0900

    update ver

---
+++ git.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "update ver"
+git push origin master
---
+++ set-up.sh
@@ -0,0 +1,5 @@
+django-admin startproject $(your_project_name)
+
+cd $(your_project_name)
+
+python manage.py startapp $(your_inapi)
